bba1cf331f37418d63c57ec4c007578a52f1eb98
polina-shimolina <shimolinapk@gmail.com>
2025-06-19 18:14:14 +0300

    fix(folder structure)

---
+++ src/components/AnalyticsComponents/FileUpload/SubmitButton/SubmitButton.tsx
@@ -0,0 +1,20 @@
+import Button from '../../../UI/Button/Button';
+import styles from './SubmitButton.module.css';
+import { useUpload as useUploadState } from '../../../../context/UploadContext/UploadContext';
+
+export default function SubmitButton({ onClick }: { onClick: () => void }) {
+    const { uploadState } = useUploadState();
+    const enabled = uploadState === 'uploaded';
+
+    return (
+        <div className={`${styles.container} ${uploadState === 'parsing' ? styles.hidden : ''}`}>
+            {uploadState !== 'parsing' && uploadState !== 'done' && (
+                <Button
+                    text="Отправить"
+                    variant={enabled ? 'primary' : 'disabled'}
+                    onClick={onClick}
+                />
+            )}
+        </div>
+    );
+}
